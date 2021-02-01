7bc418b12aaca22813b80aed5a94e3a7e7deb636
Nikolay Zainchkovskiy <extAvara@aoavant.ru>
2021-02-01 10:50:21 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <Button onClick={() => setCount((count) => count + 1)}>count is: {count}</Button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+          {" | "}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
